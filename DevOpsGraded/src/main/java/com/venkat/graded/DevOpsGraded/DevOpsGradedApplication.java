841e91b2f453aaa7e7233538ab57d29a86698002
student <mucharlavenka.navee@hcl.com>
2021-04-20 16:33:41 +0000

    first commit

---
+++ DevOpsGraded/src/main/java/com/venkat/graded/DevOpsGraded/DevOpsGradedApplication.java
@@ -0,0 +1,14 @@
+package com.venkat.graded.DevOpsGraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
